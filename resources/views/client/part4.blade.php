a1bd2350fca5aa9861c04ea0ca89d553cc725d52
Ali Azlan <aliazlan4@gmail.com>
2017-09-07 01:14:02 +0500

    user cannot select same option in part 4 for more than 4 times

--- resources/views/client/part4.blade.php
+++ resources/views/client/part4.blade.php
@@ -5,7 +5,7 @@
         <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <form class="form" method="post" action="/questioner/{{ $code }}/submit">
+            <form class="form" id="part4Form" method="post" action="/questioner/{{ $code }}/submit">
                 {{ csrf_field() }}
 
 
@@ -28,16 +28,16 @@
                             <div class="form-group">
                                 <label class="control-label" for="{{ $key+1 }}">{{ $key+1 }}. {{ $question }}</label><br/>
                                 <label class="radio-inline">
-                                    <input type="radio" name="{{ $key+1 }}" value="4" required @if($saved_input[$key+1] == "4") checked @endif> Very Important
+                                    <input type="radio" name="{{ $key+1 }}" value="4" onclick="return clicked(4, {{ $key+1 }});" required @if($saved_input[$key+1] == "4") checked @endif> Very Important
                                 </label>
                                 <label class="radio-inline">
-                                    <input type="radio" name="{{ $key+1 }}" value="3" required @if($saved_input[$key+1] == "3") checked @endif> Important
+                                    <input type="radio" name="{{ $key+1 }}" value="3" onclick="return clicked(3, {{ $key+1 }});" required @if($saved_input[$key+1] == "3") checked @endif> Important
                                 </label>
                                 <label class="radio-inline">
-                                    <input type="radio" name="{{ $key+1 }}" value="2" required @if($saved_input[$key+1] == "2") checked @endif> Somewhat Important
+                                    <input type="radio" name="{{ $key+1 }}" value="2" onclick="return clicked(2, {{ $key+1 }});" required @if($saved_input[$key+1] == "2") checked @endif> Somewhat Important
                                 </label>
                                 <label class="radio-inline">
-                                    <input type="radio" name="{{ $key+1 }}" value="1" required @if($saved_input[$key+1] == "1") checked @endif> Unimportant
+                                    <input type="radio" name="{{ $key+1 }}" value="1" onclick="return clicked(1, {{ $key+1 }});" required @if($saved_input[$key+1] == "1") checked @endif> Unimportant
                                 </label>
                             </div>
                             @if($key < count($questions) - 1)
@@ -57,4 +57,22 @@
     </div>
         </div>
 </div>
+<script>
+    var arr = [0, 0, 0, 0];
+    var old = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    function clicked(opt, row){
+        if(arr[opt-1] >= 4){
+            return false;
+        }
+        else if (old[row-1] != opt){
+            if(old[row-1] != 0){
+                arr[old[row-1]-1]--;
+            }
+            old[row-1] = opt;
+            arr[opt-1]++;
+            return true;
+        }
+    }
+</script>
 @endsection
