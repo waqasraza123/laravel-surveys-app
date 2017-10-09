94e63a4553dfc3081097e7e39196bfb4fb792f39
Ali Azlan <aliazlan4@gmail.com>
2017-10-09 22:47:51 +0500

    navbar editing complete

--- resources/views/layouts/topnavbar.blade.php
+++ resources/views/layouts/topnavbar.blade.php
@@ -2,9 +2,19 @@
     <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
         <div class="navbar-header">
             {{--<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>--}}
-            <p style="margin: 0px; padding: 0px 25px; line-height: 60px">Firm Name - |
+            <p style="margin: 0px; padding: 0px 25px; line-height: 60px">
+                <?php if(Auth::user()->role == "advisor")
+                    echo "Firm Name - " . \App\User::where("code", Auth::user()->firm_code)->get()->first()->name . "|";
+                ?>
                 Financial Practice ID - <?php if(Auth::user()->role == "firm") echo Auth::user()->code; else if(Auth::user()->role == "advisor") echo Auth::user()->firm_code; ?> |
-                Firm Registration Date -  </p>
+                Firm Registration Date -
+                <?php
+                    if(Auth::user()->role == "firm")
+                        echo date("F j, Y", strtotime(Auth::user()->created_at));
+                    else if(Auth::user()->role == "advisor")
+                        echo date("F j, Y", strtotime(\App\User::where("code", Auth::user()->firm_code)->get()->first()->created_at));
+                ?>
+            </p>
         </div>
         <ul class="nav navbar-top-links navbar-right">
 
