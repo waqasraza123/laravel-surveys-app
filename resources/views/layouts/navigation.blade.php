2548b39cdc91510f1705b2aedd4994a680f880b3
Ali Azlan <aliazlan4@gmail.com>
2017-11-03 22:11:52 +0500

    active submenu color changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,8 +15,8 @@
                     <li class="treeview @if (Request::is('advisors/*')) active @endif">
                         <a href="#"><i class="fa fa-users"></i> <span class="nav-label">Advisers</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
-                            <li><a href="/advisors/approved">Active Advisers</a></li>
-                            <li><a href="/advisors/new">Pending Approval</a></li>
+                            <li class="@if (Request::is('advisors/approved')) active @endif"><a href="/advisors/approved">Active Advisers</a></li>
+                            <li class="@if (Request::is('advisors/new')) active @endif"><a href="/advisors/new">Pending Approval</a></li>
                         </ul>
                     </li>
 
@@ -33,8 +33,8 @@
                     <li class="treeview @if (Request::is('advisors/*')) active @endif">
                         <a href="#"><i class="fa fa-users"></i> <span class="nav-label">Advisers</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
-                            <li><a href="/advisors/approved">Active Advisers</a></li>
-                            <li><a href="/advisors/new">Pending Approval</a></li>
+                            <li class="@if (Request::is('advisors/approved')) active @endif"><a href="/advisors/approved">Active Advisers</a></li>
+                            <li class="@if (Request::is('advisors/new')) active @endif"><a href="/advisors/new">Pending Approval</a></li>
                         </ul>
                     </li>
 
@@ -45,9 +45,9 @@
                     <li class="treeview @if (Request::is('tokens/*')) active @endif">
                         <a href="#"><i class="fa fa-dollar"></i> <span class="nav-label">Tokens</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level collapse">
-                            <li><a href="/tokens/purchase">Purchase</a></li>
-                            <li><a href="/tokens/history">Purchase History</a></li>
-                            {{--<li><a href="/tokens/usage">Usage History</a></li>--}}
+                            <li class="@if (Request::is('tokens/purchase')) active @endif"><a href="/tokens/purchase">Purchase</a></li>
+                            <li class="@if (Request::is('tokens/history')) active @endif"><a href="/tokens/history">Purchase History</a></li>
+                            {{--<li class="@if (Request::is('tokens/usage')) active @endif"><a href="/tokens/usage">Usage History</a></li>--}}
                         </ul>
                     </li>
 
@@ -62,8 +62,8 @@
                      <li class="treeview @if (Request::is('reports/*')) active @endif">
                         <a href="/reports/new"><i class="fa fa-file-text"></i> <span>Investor DNA Profile </span> <i class="fa fa-angle-left pull-right"></i></a>
                         <ul class="nav nav-second-level collapse">
-                            <li><a href="{{url('/reports/new')}}">Add New</a></li>
-                            <li><a href="{{url('/reports/view')}}">View </a></li>
+                            <li class="@if (Request::is('reports/new')) active @endif"><a href="{{url('/reports/new')}}">Add New</a></li>
+                            <li class="@if (Request::is('reports/view')) active @endif"><a href="{{url('/reports/view')}}">View </a></li>
                         </ul>
                     </li>
                     <li @if (Request::is('profile')) class="active" @endif><a href="{{url('/profile')}}"><i class="fa fa-user"></i> <span>Adviser Profile</span></a></li>
