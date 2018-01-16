84e709524f7241a35bba5904631b31cd3076d781
Aaron Busary <aaron@innsite.com.au>
2018-01-17 00:28:09 +1100

    fix invoice

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,8 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '/usr/bin/wkhtmltopdf',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        //'binary'  => '/usr/bin/wkhtmltopdf',
          //       'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
         'timeout' => false,
         'options' => array(),
